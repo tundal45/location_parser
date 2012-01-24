125b797684b26ef2a9a289136db68b6f6a557c03
Ashish Dixit <tundal45@gmail.com>
2012-01-23 21:18:34 -0600

    Added ability to parse comma or space delimeted City & State

--- lib/location_parser.rb
+++ lib/location_parser.rb
@@ -1,3 +1,19 @@
+require 'ostruct'
+
 class LocationParser
-  
+  def initialize(location_string)
+    @location_string = location_string
+  end
+
+  def parse
+    location = OpenStruct.new
+
+    if @location_string.include?(',')
+      location.city, location.state = @location_string.split(",").map(&:strip)
+    else
+      city_state_regex = /(\w+(?:\s*\w+))+\s(\w{2})$/
+      location.city, location.state = city_state_regex.match(@location_string)[1..2]
+    end
+    location
+  end
 end
--- spec/location_parser_spec.rb
+++ spec/location_parser_spec.rb
@@ -2,15 +2,75 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require_relative '../lib/location_parser'
 
 describe LocationParser do
-  it "should handle locations with comma separated city & state information"
-  it "should handle locations with space separated city & state information"
-  it "should handle locations that specify two or more preferred cities"
-  it "should handle locations that include country"
-  it "should handle locations that include zipcode"
-  it "should handle locations listed as 'Anywhere'"
-  it "should handle non-US locations"
-  it "should handle two or more locations separated by 'or'"
-  it "should handle locations that list comma separeted US states"
-  it "should handle locations with neighborhood information"
-  it "should handle locations with region information"
+  describe "locations with city & state information" do
+    context "comma separated" do
+      it "parses city & state information" do
+        location_string = "New York, NY"
+        parser = LocationParser.new(location_string)
+        location = parser.parse
+        location.city.should == "New York"
+        location.state.should == "NY"
+      end
+    end
+
+    context "space separated" do
+      it "parses city & state information" do
+        location_string = "San Francisco CA"
+        parser = LocationParser.new(location_string)
+        location = parser.parse
+        location.city.should == "San Francisco"
+        location.state.should == "CA"
+      end
+    end
+  end
+
+  describe "locations that specify two or more cities" do
+    context "contains 'preferred'" do
+      it "parses cities"
+    end
+  end
+
+  describe "locations with country information" do
+    context "just country name" do
+      it "parses the country name"
+    end
+
+    context "full US address" do
+      it "parses address, city, state, country and zipcode"
+    end
+  end
+
+  describe "locations that include zipcode" do
+    it "parses city, state, country and zipcode"
+  end
+
+  describe "locations listed as 'Anywhere'" do
+
+  end
+
+  describe "two or more locations separated by 'or'" do
+    it "parses city & state from all locations"
+  end
+
+  describe "locations that list comma separeted US states" do
+    it "parses all states"
+  end
+
+  describe "locations with neighborhood information" do
+    context "state information included" do
+      it "parses neighborhood, city and state"
+    end
+    context "no state information" do
+      it "parses neighborhood and city"
+    end
+  end
+
+  describe "locations with region information" do
+    context "state information included" do
+      it "parses city, state and region"
+    end
+    context "no state information" do
+      it "parses city and region"
+    end
+  end
 end
